b8b8748f87494bc1a1782034523876fb4a9db4c3
Anna Klanicova <aklanicova15@gmail.com>
2025-10-21 16:43:47 +0200

    stylistické vylepšení - přidání +=

--- hw01_output.py
+++ hw01_output.py
@@ -15,7 +15,7 @@ for znak_textu in text:
     if znak_textu not in znak_counts:
         znak_counts[znak_textu] = 1
     else:
-        znak_counts[znak_textu] = znak_counts[znak_textu] + 1
+        znak_counts[znak_textu] += 1
 
 with open('hw01_output.json', 'w', encoding='utf-8') as f:
     json.dump(znak_counts, f, ensure_ascii=False, indent=4, sort_keys=True)
