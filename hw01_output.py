28a2a5e5ba506adb287347800f587fa9600f5594
Anna Klanicova <aklanicova15@gmail.com>
2025-10-21 16:24:03 +0200

    Přidání hw01_output.py

--- hw01_output.py
+++ hw01_output.py
@@ -1,7 +1,7 @@
 import json
 
 
-with open('c:\\Users\\anna\\Documents\\alice.txt','r', encoding='utf-8') as file:
+with open('alice.txt','r', encoding='utf-8') as file:
     text = file.read()
 
 text = text.lower()
@@ -13,9 +13,9 @@ znak_counts = {}
 
 for znak_textu in text:
     if znak_textu not in znak_counts:
-        znak_counts[znak_textu] = 0
+        znak_counts[znak_textu] = 1
     else:
         znak_counts[znak_textu] = znak_counts[znak_textu] + 1
 
 with open('hw01_output.json', 'w', encoding='utf-8') as f:
-    json.dump(znak_counts, f, ensure_ascii=False, indent=4)
+    json.dump(znak_counts, f, ensure_ascii=False, indent=4, sort_keys=True)
